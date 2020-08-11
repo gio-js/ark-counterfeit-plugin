41713770bb5cf152ac342b8d238b8006bad9d010
gio-js <g.sorgente86@gmail.com>
2020-08-11 11:47:08 +0200

    review packages

--- src/plugin.ts
+++ src/plugin.ts
@@ -9,9 +9,10 @@ import {
 export const plugin: Container.IPluginDescriptor = {
     pkg: require("../package.json"),
     defaults,
-    alias: "my-custom-transactions",
+    alias: "ark-counterfeit-transactions",
     async register(container: Container.IContainer, options) {
         container.resolvePlugin<Logger.ILogger>("logger").info("Registration ark-anticounterfeit-transactions started...");
+
         Handlers.Registry.registerTransactionHandler(RegisterManufacturerTransactionHandler);
         Handlers.Registry.registerTransactionHandler(RegisterProductTransactionHandler);
 
